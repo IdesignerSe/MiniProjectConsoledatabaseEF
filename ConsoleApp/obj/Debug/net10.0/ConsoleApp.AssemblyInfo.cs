8f75145e129ea8f128dcb7535c3a90801ecdcb6d
idesignerSe <hebera75@gmail.com>
2026-07-14 10:39:59 +0200

    Migrations

--- ConsoleApp/obj/Debug/net10.0/ConsoleApp.AssemblyInfo.cs
+++ ConsoleApp/obj/Debug/net10.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b64bb6687767e450d8260a0245c2fc9fe0b2c49f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0722520052739d66d32d0051b95a0e2845eb8826")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
