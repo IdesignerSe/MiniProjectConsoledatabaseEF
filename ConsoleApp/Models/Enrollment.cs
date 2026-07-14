29824589d09f548e47dd1328971660b9bae1b776
idesignerSe <hebera75@gmail.com>
2026-07-14 10:59:19 +0200

    update

--- ConsoleApp/Models/Enrollment.cs
+++ ConsoleApp/Models/Enrollment.cs
@@ -3,10 +3,10 @@ namespace ConsoleApp.Models
     public class Enrollment
     {
         public int StudentId { get; set; }
-        public Student Student { get; set; }
+        public Student? Student { get; set; }
 
         public int CourseId { get; set; }
-        public Course Course { get; set; }
+        public Course? Course { get; set; }
 
         public string? Grade { get; set; }
     }
