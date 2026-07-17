d95fa9442499cac506c8094ae16d92507d028f3d
idesignerSe <hebera75@gmail.com>
2026-07-17 09:06:27 +0200

    Profile.cs

---
+++ ApiServer/Models/Profile.cs
@@ -0,0 +1,10 @@
+namespace ApiServer.Models
+{
+    public class Profile
+    {
+        public int Id { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public int StudentId { get; set; }
+    }
+}
